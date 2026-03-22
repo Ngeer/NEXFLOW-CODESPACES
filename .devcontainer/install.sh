5759335ce764ff971e153930bfcd475fa425dcf8
Stuart Andrew Ngereza <113860839+Ngeer@users.noreply.github.com>
2026-03-22 11:13:20 +0000

    Add Trim Galore and cutadapt

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 conda install -y -c bioconda -c conda-forge subread multiqc openjdk=17
+pip install cutadapt
+curl -fsSL https://github.com/FelixKrueger/TrimGalore/archive/0.6.10.tar.gz | tar xz
+mv TrimGalore-0.6.10/trim_galore /usr/local/bin/
 wget -qO- https://get.nextflow.io | bash
 mv nextflow /usr/local/bin/
 echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
