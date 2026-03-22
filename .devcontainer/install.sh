0feba63be669d5486f1ea4a6ea5e96fe0ce23dd1
Stuart Andrew Ngereza <113860839+Ngeer@users.noreply.github.com>
2026-03-22 11:07:52 +0000

    Nextflow 25, Java 17, featureCounts, MultiQC all working

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 set -e
-conda install -y -c bioconda -c conda-forge subread multiqc
+conda install -y -c bioconda -c conda-forge subread multiqc openjdk=17
+wget -qO- https://get.nextflow.io | bash
+mv nextflow /usr/local/bin/
+echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
