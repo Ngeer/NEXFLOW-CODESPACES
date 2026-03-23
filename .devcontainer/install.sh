55f89a7e556d455224f445ccae950be7e3549be2
Stuart Andrew Ngereza <113860839+Ngeer@users.noreply.github.com>
2026-03-23 18:48:17 +0000

    Clean install script with all bioinformatics tools

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 set -e
+
+# Java
 curl -s "https://get.sdkman.io" | bash
 source ~/.sdkman/bin/sdkman-init.sh
 sdk install java 17.0.10-tem
-conda install -y -c bioconda -c conda-forge subread multiqc star fastqc
+
+# All bioinformatics tools via conda
+conda install -y -c bioconda -c conda-forge \
+  fastqc multiqc star subread \
+  snippy spades iqtree trimmomatic
+
+# Cutadapt
 pip install cutadapt
+
+# TrimGalore
 curl -fsSL https://github.com/FelixKrueger/TrimGalore/archive/0.6.10.tar.gz | tar xz
 mv TrimGalore-0.6.10/trim_galore /usr/local/bin/
+
+# Nextflow
 wget -qO- https://get.nextflow.io | bash
 mv nextflow /usr/local/bin/
+
 echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
 echo 'source ~/.sdkman/bin/sdkman-init.sh' >> ~/.bashrc
